d011e729d1733c66110d412a4f8a6e9984752826
Dalibor Skocibusic <skody001@mymail.unisa.edu.au>
2022-07-31 22:36:20 +0930

    Basically works, needs touching up

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-let searchTerms = "Harry Potter";
+let searchTerms = "";
 // let APIKey = "AIzaSyB_tWPK8L8Uv5EAk7nYYbhflEuE8Gq5JMo";
-const myFetch = `https://www.googleapis.com/books/v1/volumes?q=${searchTerms}`;
+let myFetch = `https://www.googleapis.com/books/v1/volumes?q=${searchTerms}`;
 
 // https://www.googleapis.com/books/v1/volumes?q=Harry%20Potter
 
@@ -13,92 +13,137 @@ const myFetch = `https://www.googleapis.com/books/v1/volumes?q=${searchTerms}`;
 //         console.log("results are " + results);
 //     });
 
+const updateDOM = (i, bookInfo) => {
+    let idString = "";
+    let idStringAuthor = "";
+    let idStringDescription = "";
+    let imageString = "image";
+
+    switch (i) {
+        case 0: {
+            (idString = "result0"), (imageString = "image0");
+            break;
+        }
+        case 1: {
+            (idString = "result1"), (imageString = "image1");
+            break;
+        }
+        case 2: {
+            (idString = "result2"), (imageString = "image2");
+            break;
+        }
+        case 3: {
+            (idString = "result3"), (imageString = "image3");
+            break;
+        }
+        case 4: {
+            (idString = "result4"), (imageString = "image4");
+            break;
+        }
+        case 5: {
+            (idString = "result5"), (imageString = "image5");
+            break;
+        }
+        case 6: {
+            (idString = "result6"), (imageString = "image6");
+            break;
+        }
+        case 7: {
+            (idString = "result7"), (imageString = "image7");
+            break;
+        }
+        case 8: {
+            (idString = "result8"), (imageString = "image8");
+            break;
+        }
+        case 9: {
+            (idString = "result9"), (imageString = "image9");
+            break;
+        }
+    }
+    idStringAuthor = idString + "__author";
+    idStringDescription = idString + "__description";
+
+    document.getElementById(idString).textContent = "Title: " + bookInfo.title;
+
+    // Authors
+    updateAuthors(bookInfo.authors, idStringAuthor);
+
+    // Description
+    updateDesc(bookInfo.description, idStringDescription);
+
+    // image
+    updateImage(bookInfo.imageLinks.smallThumbnail, imageString);
+};
+
+const updateTitle = (title) => {
+    document.getElementById(idString).textContent = "Title: " + title;
+};
+
+const updateAuthors = (authors, idStringAuthor) => {
+    if (authors) {
+        document.getElementById(idStringAuthor).textContent =
+            "Authors: " + authors;
+    } else {
+        document.getElementById(idStringAuthor).textContent =
+            "Somehow, there were no authors: ";
+    }
+};
+
+const updateDesc = (desc, idStringDescription) => {
+    if (desc) {
+        let catDesc = desc.slice(0, 111) + "...";
+        document.getElementById(idStringDescription).textContent =
+            "Description: " + catDesc; //results[i].description;
+    } else {
+        document.getElementById(idStringDescription).textContent =
+            "No description available"; //results[i].description;
+    }
+};
+
+const updateImage = (image, imageString) => {
+    if (image) {
+        document.getElementById(imageString).src = image;
+    }
+};
+
+const clearDOM = () => {
+    // document.getElementsByClassName("result").value = 0;
+};
+
 async function fetchUser(url) {
     const response = await fetch(url);
-    console.log("Response " + response);
-
     const data = await response.json();
 
     let results = [];
 
-    for (let i = 0; i < 10; i++) {
-        results[i] = data.items[i].volumeInfo;
-    }
-
-    let a = data.items[0];
-    let b = data.items[1];
-
-    let i = 5;
-    // Title
-    document.getElementById("result10").textContent = "Title: " + results[i].title;
-
-    // Authors
-    document.getElementById("result10__author").textContent =
-        "Authors: " + results[i].authors;
-
-    // Description
-    let catDesc = results[i].description.slice(0, 111) + "...";
-    document.getElementById("result10__description").textContent =
-        "Description: " + catDesc //results[i].description;
-
-    // Needs image, author, title and description
-
-    // image
-    document.getElementById("image10").src =
-        results[i].imageLinks.smallThumbnail;
-
-    // worls
-    //console.log("Data " + JSON.parse(JSON.stringify(data)));
-
-    // Tyring this
-    return data;
-
-    // THis works
-    // return JSON.stringify(data);
-    // results.then((item) => console.log(item));
-    // console.log("Response as json " + results);
-    // response.then((item) => {
-    //     console.log(item);
-    // });
-    // const { title } = results[0].kind;
-    // results.then((value) => console.log(value));
-
-    // data.then((value) => console.log(value));
-    // document.querySelector("h2").innerText = `${results}`;
-
-    // return response;
-
-    // const { results } = await response.json();
-    // console.log("Response after JSON is" + results);
-    // const { results } = data;
-    // const name = results[0]
-    // const {title, first, last} = name
-    // const { title, first, last } = results[0].name;
-    // document.querySelector("h2").innerText = `${title} ${first} ${last}`;
-    // console.log("results are " + results);
-    // return data;
+    if (data) {
+        if (data.items) {
+            if (data.items.length >= 10) {
+                for (let i = 0; i < 9; i++) {
+                    results[i] = data.items[i].volumeInfo;
+                    updateDOM(i, results[i]);
+                }
+            }
+        }
+    } else clearDOM();
+    return results;
 }
 
-let repval = fetchUser(myFetch);
-
-const evalResult = async (input) => {
-    console.log("Eval being called. Input is: " + input);
-    let retArr = input.then((value) => {
-        console.log(value);
-        value.map((item) => item.toUpperCase());
-    });
-};
-// const procval = repval.then((value) => {
-//     console.log(value);
-//     const { results } = value;
-// });
-
-// => value.map((item) => item)); //console.log(value));
-// fetchUser();
-
 document.querySelector("button").addEventListener("click", () => {
-    // console.log("Clicked the button");
-    // console.log("Value entered into myFetch: " + myFetch);
-    repval = fetchUser(myFetch);
-    // repval = evalResult(repval);
+    event.preventDefault();
+    searchTerms = document.getElementById("searchString").value;
+    console.log(document.getElementById("searchString").value);
+    myFetch = `https://www.googleapis.com/books/v1/volumes?q=${searchTerms}`;
+
+    if (searchTerms) {
+        repval = fetchUser(myFetch);
+    } else {
+        alert("Please enter some search terms");
+    }
 });
+
+// Adding modal feature
+// document.getElementById("result0").addEventListener("click", () => {
+//     event.preventDefault();
+// });
